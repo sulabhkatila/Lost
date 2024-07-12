130364214f6a9f948c92ab7179e514102fb99ca1
Sulabh Katila <sulabhkatila@gmail.com>
2024-07-12 13:51:45 -0400

    Define Token

--- src/token.rs
+++ src/token.rs
@@ -1,7 +1,74 @@
+#[derive(Debug)]
 pub enum TokenType {
+    // Single-character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    SemiColon,
+    Minus,
+    Plus,
+    Slash,
+    Star,
 
+    // One or two character tokens
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals
+    Identifier,
+    String,
+    Number,
+
+    // Keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    EOF,
 }
 
+#[derive(Debug)]
 pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Option<String>,
+    line: usize,
+}
 
+impl Token {
+    pub fn new(
+        token_type: TokenType,
+        lexeme: String,
+        literal: Option<String>,
+        line: usize,
+    ) -> Token {
+        Token {
+            token_type: token_type,
+            lexeme: lexeme,
+            literal: literal,
+            line: line,
+        }
+    }
 }
